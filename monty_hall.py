a052c3c41da8d278d1c3c957dc2d4a0cc1fad2a6
Saksham Jaiswal <sakshamjswl45@gmail.com>
2022-09-23 13:19:29 +0530

    added a function to monty_hall.py, which allows to the choice of swithching or not switching

--- monty_hall.py
+++ monty_hall.py
@@ -3,27 +3,32 @@ import random
 prize_results = []
 prizes = [0, 1, 2]
 
-trials = 10**5
+TRIALS = 10**5
 
-for i in range(trials):
+for i in range(TRIALS):
     prize_results.append(random.choice(prizes))
-    
-res = 0
-for i in range(trials):
-    doors = [0, 1, 2]
-    choice = random.choice(doors)
-    # remove one option with no prize
-    for door in doors:
-        if door != choice and door != prize_results[i]:
-            reveal_door = door
-            break
-    doors.pop(reveal_door)
-    # change choice
-    choice = random.choice(doors)
+  
+def solve(trials, change = False):
+    res = 0
+    for i in range(trials):
+        doors = [0, 1, 2]
+        choice = random.choice(doors)
+        # remove one option with no prize
+        for door in doors:
+            if door != choice and door != prize_results[i]:
+                reveal_door = door
+                break
+        doors.pop(reveal_door)
+        # change choice
+        if change:
+            choice = random.choice(doors)
+                
+        if choice == prize_results[i]:
+            res += 1
             
-    if choice == prize_results[i]:
-        res += 1
+    return res/trials
     
-        
-        
-print(res/trials)
+    
+
+print(f'Ratio of wins when choosing not to switch: {solve(TRIALS):.2f}')
+print(f'Ratio of wins when choosing to switch: {solve(TRIALS, True):.2f}')
